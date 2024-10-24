da0cfadc6f555e5197f050465d6a3add4bfaafb4
RasmusStJa <PN83DC@student.aau.dk>
2024-10-24 16:54:23 +0200

    idk just look at the commit chagnes

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <stdbool.h>
 
 #define WIDTH 3
-#define HEIGHT WIDTH
+#define HEIGHT 3
 
 typedef enum { empty, X, O } XO;
 
-XO arena[HEIGHT][WIDTH];
+XO arena[WIDTH][HEIGHT];  //2D array initialized as empty (because it's the first enum in XO
 
-XO player = X;
-XO winner = empty;
+XO player = X;  //x starts
+XO winner = empty;  //winner has not been determined
 
-XO checkWin(XO arena[HEIGHT][WIDTH]);
+XO checkWin(XO arena[WIDTH][HEIGHT]);
 
-void print2DArr(XO arena[HEIGHT][WIDTH]);
+void print2DArr(XO arena[WIDTH][HEIGHT]);
 
 bool check3equals(XO one, XO two, XO three);
 
@@ -35,16 +35,20 @@ int main(void)
                 break;  //hopefully break while loop
         }
 
-        //get coords
+        //get coords (from 1 - 3)
         scanf("%d %d", &x, &y);
-        x--; y--;
+        x--; y--;  //change to 0 - 2
 
-        //set coords
-        if (arena[y][x] == empty) {
-            arena[y][x] = player;
+        //failsafe: make sure the area isn't already taken
+        if (arena[x][y] == empty) {
 
+            //place X/O at chosen location
+            arena[x][y] = player;
+
+            //update winner
             winner = checkWin(arena);
 
+            //switch player
             if (player == X){ player = O; }
             else { player = X; }
 
@@ -66,7 +70,8 @@ int main(void)
     return 0;
 }
 
-XO checkWin(XO arena[HEIGHT][WIDTH]) {
+XO checkWin(XO arena[WIDTH][HEIGHT]) {
+    //NOTE: will not work with any arena-sizes BUT 3x3
     for (int i = 0; i < HEIGHT; i++) {
         if (check3equals(arena[i][0], arena[i][1], arena[i][2])){ //horizontal
             return arena[i][0];
@@ -84,10 +89,10 @@ XO checkWin(XO arena[HEIGHT][WIDTH]) {
     return empty;
 }
 
-void print2DArr(XO arena[HEIGHT][WIDTH]) {
+void print2DArr(XO arena[WIDTH][HEIGHT]) {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            switch (arena[y][x]) {
+            switch (arena[x][y]) {
                 case X:
                     printf("X");
                     break;
